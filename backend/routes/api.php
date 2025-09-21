1b78696b038fc48baad93ddb5a91c13273666866
AkbariAidul <akbariaidul@gmail.com>
2025-09-21 21:07:24 +0800

    memindahkan folder yang seharusnya difrontend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,9 +1,11 @@
 <?php
 
-use App\Http\Controllers\ProjectController;
-use App\Http\Controllers\TaskController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('projects', ProjectController::class);
-    Route::apiResource('tasks', TaskController::class);
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    $user = $request->user();
+    // Ambil semua nama permission milik user dan sisipkan ke dalam response
+    $user->permissions = $user->getAllPermissions()->pluck('name');
+    return $user;
 });
